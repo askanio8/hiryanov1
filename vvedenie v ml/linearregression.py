b90d704310047da2f685cd745da016e166d51788
askanio8@gmail.com <askanio8@gmail.com>
2021-03-28 08:54:24 +0300

    apd

--- vvedenie v ml/linearregression.py
+++ vvedenie v ml/linearregression.py
@@ -1,7 +1,8 @@
 import pandas as pd
-import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction import DictVectorizer
+from scipy.sparse import hstack
+from sklearn.linear_model import Ridge
 
 traindata = pd.read_csv('salary-train.csv')
 testdata = pd.read_csv('salary-test-mini.csv')
@@ -16,19 +17,37 @@ testdata['FullDescription'] = testdata['FullDescription'].replace('[^a-zA-Z0-9]'
 
 # Из столбца FullDescription делаем тысячи столбцов слов, которые встречаются в более чем 5 объектах
 # Их значения - взвешенное среднее (TF-IDF)
-# Результат - матрица а не DataFrame
+# Результат - матрица, а не DataFrame
 vectorizer = TfidfVectorizer(min_df=5)
-X = vectorizer.fit_transform(traindata['FullDescription'])
-Y = vectorizer.transform(testdata['FullDescription'])
+trainFullDescriptionVectorizer = vectorizer.fit_transform(traindata['FullDescription'])
+testFullDescriptionVectorizer = vectorizer.transform(testdata['FullDescription'])
 
 # Заполняем nan пропуски значений в столбцах LocationNormalized и ContractTime
-traindata['LocationNormalized'] = traindata['LocationNormalized'].fillna('nan', inplace=True)
-testdata['LocationNormalized'] = testdata['LocationNormalized'].fillna('nan', inplace=True)
-traindata['ContractTime'] = traindata['ContractTime'].fillna('nan', inplace=True)
-testdata['ContractTime'] = testdata['ContractTime'].fillna('nan', inplace=True)
+traindata['LocationNormalized'].fillna('nan', inplace=True)
+testdata['LocationNormalized'].fillna('nan', inplace=True)
+traindata['ContractTime'].fillna('nan', inplace=True)
+testdata['ContractTime'].fillna('nan', inplace=True)
 
-# Из двух столбцов категориальных признаков LocationNormalized и ContractTime делаем много столбцов -
-# для каждого варианта признака выделяем отдельный столбец со значением 0 или 1.
+# Из двух столбцов категориальных признаков LocationNormalized и ContractTime двумерную матрицу -
+# для каждого варианта признака выделяем отдельный признак со значением 0 или 1.
 # Это называется one-hot-кодирование. Используем DictVectorizer
+# Результат двумерная матрица. Строки - объекты, первые три столбца - ContractTime, последние 1763 столбца - локации
+# Если в конструктор передать sparse=False, то результатом будет не scipy.sparse.csr_matrix а numpy.ndarray
+# в ndarray лучше предпросмотр
+dvectorizer = DictVectorizer()  # можно передать sparse=False
+# Строка ниже переводит два столбца в словарь вида: строка->{LocationNormalized:London, ContractTime: permanent}...
+# argument = traindata[['LocationNormalized', 'ContractTime']].to_dict('records')
+trainLocationContractVecrorizer = dvectorizer.fit_transform(traindata[['LocationNormalized',
+                                                                       'ContractTime']].to_dict('records'))
+testLocationContractVecrorizer = dvectorizer.transform(testdata[['LocationNormalized',
+                                                                 'ContractTime']].to_dict('records'))
 
-print(Y)
+# Объединяем матрицы признаков
+trainMatrix = hstack([trainFullDescriptionVectorizer, trainLocationContractVecrorizer])
+testMatrix = hstack([testFullDescriptionVectorizer, testLocationContractVecrorizer])
+
+# Обучаем, предсказываем
+clf = Ridge(alpha=1, random_state=241)  # alpha - сила регуляризации, 1/2*С в других моделях
+clf.fit(trainMatrix, traindata.filter(items=['SalaryNormalized']))
+result = clf.predict(testMatrix)
+print(result)
